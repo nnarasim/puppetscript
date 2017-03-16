7709c52b907a612f1cc4e29c62137df832d93420
nnarasim <narendran.lnt@gmail.com>
2017-03-17 00:03:37 +0530

    fixed doubles quote in exec

--- puppet/modules/core/manifests/init.pp
+++ puppet/modules/core/manifests/init.pp
@@ -130,7 +130,7 @@ class core::install {
 class core::java {
  exec { "download-java" :
         cwd     => "/opt",
-        command => "wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u112-b15/jdk-8u112-linux-x64.tar.gz",
+        command => "wget --no-check-certificate --no-cookies --header \" Cookie: oraclelicense=accept-securebackup-cookie \" http://download.oracle.com/otn-pub/java/jdk/8u112-b15/jdk-8u112-linux-x64.tar.gz",
 		mode => "755"
         
     }
