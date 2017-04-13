3ec5310a822bec489026445d7a74d11b7ddeac5d
nnarasim <narendran.lnt@gmail.com>
2017-04-13 14:19:14 +0530

    shred

--- puppet/modules/core/files/javahome.sh
+++
@@ -1,9 +0,0 @@
-# Add /opt/jdk1.8.0_112/bin to the path for sh compatible users
-
-if ! echo $JAVA_HOME | grep -q /opt/jdk1.8.0_112 ; then
-  export JAVA_HOME=$JAVA_HOME:/opt/jdk1.8.0_112
-fi
-
-if ! echo $PATH | grep -q /opt/jdk1.8.0_112/bin ; then
-  export PATH=$PATH:/opt/jdk1.8.0_112/bin
-fi
